02f8eba88752d1067bcf1368aa68e0a87a3aed03
majorcodeX <neon.ratemo@gmail.com>
2025-05-12 18:31:27 +0300

    First python project

---
+++ quizgame.py
@@ -0,0 +1,40 @@
+print("Welcome to my web development quiz")
+
+playing = input("Do you want to play? ").lower()
+
+if playing != "yes":
+    quit()
+
+print("Alright let's play :)")
+score = 0
+
+question = input("What does CPU stand for? ").lower()
+if question == "central processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("That's incorrect, please try again")
+
+question = input("What does GPU stand for? ").lower()
+if question == "graphics processing unit":
+    print('Correct!')
+    score += 1
+else:
+    print("That's incorrect, please try again")
+
+question = input("What does RAM stand for? ").lower()
+if question == "random access memory":
+    print('Correct!')
+    score += 1
+else:
+    print("That's incorrect, please try again")
+
+question = input("What does PSU stand for? ").lower()
+if question == "power supply unit":
+    print('Correct!')
+    score += 1
+else:
+    print("That's incorrect, please try again")
+
+print("You got " + str(score) + " questions correct!")
+print("You got " + str((score/4) * 100) + " %")
