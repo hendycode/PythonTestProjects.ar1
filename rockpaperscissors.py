f6e214b48161868dc2d4196e3a5e717987e90155
majorcodeX <neon.ratemo@gmail.com>
2025-05-12 21:32:02 +0300

    wrap-up

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -22,6 +22,29 @@ while True:
     if user_input == "rock" and computer_pick == "scissors":
         print("You won!")
         userwins += 1
+        continue
 
+    elif user_input == "paper" and computer_pick == "rock":
+        print("You won!")
+        userwins += 1
+        continue
 
+    elif user_input == "scissors" and computer_pick == "paper":
+        print("You won!")
+        userwins += 1
+        continue
+
+    elif user_input == "rock" and computer_pick == "scissors":
+        print("You won!")
+        userwins += 1
+        continue
+
+    else:
+        print("You lost!")
+        compwins += 1
+
+    
+
+print("You won", userwins, "times")
+print("The computer won", compwins, "times.")
 print("Goodbye!")
