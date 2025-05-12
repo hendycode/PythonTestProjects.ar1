8b6f7a739761b39128582d5485e0754f5aaa571e
majorcodeX <neon.ratemo@gmail.com>
2025-05-12 19:54:11 +0300

    Guesser game

---
+++ guesser.py
@@ -0,0 +1,30 @@
+import random
+
+top_range = input("Type a number: ")
+
+if top_range.isdigit():
+    top_range = int(top_range)
+
+    if top_range <= 0:
+        print("Please type a number larger than 0")
+        quit()
+else:
+    print("Please type a number next time")
+    quit()
+
+r = random.randint(0, top_range)
+
+
+while True:
+    user_guess = input("Make a guess: ")
+    if user_guess.isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Please type a number next time")
+        continue
+
+    if user_guess == r:
+        print("You got it!")
+        break
+    else:
+        print("You got it wrong!")
