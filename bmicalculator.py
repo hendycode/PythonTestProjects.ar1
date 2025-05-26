eda306465e9b5dac071f548bcd2d1c25c7d3e081
hendycode <neonratemo@gmail.com>
2025-05-26 11:49:48 +0300

    letsgooo

--- bmicalculator.py
+++ bmicalculator.py
@@ -1,18 +1,13 @@
-your_weight = float(input("Hi there, how much do you weigh?: "))
-print(f"Weight: {your_weight}kg")
+your_weight = float(input("Please input your Weight: "))
+your_height = float(input("Please input your Height (M): "))
 
-your_height = float(input("What is your height? M: "))
-print("Weight: " + str(your_weight) + "kg")
-print("Height: " + str(your_height) + "m")
+bmi = your_weight / (your_height ** 2)
 
-bmi = your_weight / your_height ** 2
-
-
-if bmi >= 25 and bmi <= 29.9:
-    print(f"Your BMI is: {bmi}. You are Overweight.")
+if bmi > 30 :
+    print(f"Your BMI is {bmi:.4}.\nYou are obese.")
+elif bmi >= 25 and bmi <= 29.9:
+    print(f"Your BMI is {bmi:.4}.\nYou are overweight.")
 elif bmi >= 18.5 and bmi <= 24.9:
-    print(f"Your BMI is: {bmi}. Your weight is healthy")
-elif bmi < 18.5:
-    print(f"Your BMI is: {bmi}. You are underweight")
+    print(f"Your BMI is {bmi:.4}.\nYour weight is healthy.")
 else:
-    print(f"Your BMI is: {bmi}. You are Obese.")
+    print(f"Your BMI is {bmi:.4}.\nYou are underweight.")
